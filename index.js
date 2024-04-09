be48cdde9fe0846457f16cbb25321d8cef7766d0
WRD <dandobilly@gmail.com>
2024-04-09 01:02:36 -0400

    added code for initializing the readme and in the process of creating the rendering for license info

--- index.js
+++ index.js
@@ -1,13 +1,74 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const licenses = ['No license', 'MIT', 'BSD', 'Apache', 'GPL'];
+
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of the Project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Can you give a description of your project?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install the project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this app?'
+    },
+    {
+        type: 'input',
+        name: 'contributions',
+        message: 'How would you prefer others to help contribute to this project?',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What do you use to test this project?'
+    },
+    {
+        type: 'list',
+        name: 'licenseOptions',
+        message: 'Which License are you using?',
+        choices: licenses
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your github username.'
+    },
+    {
+        type: 'input',
+        name: 'githubLink',
+        message: 'Please enter a link to your github.'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address.'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFile(fileName, data, (err) => (
+        err ? console.error(err) : console.log('README.md has been successfully created!')
+    ));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.createPromptModule(questions).then((data) => writeToFile('README.me', generateMarkdown(data)));
+};
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,12 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'Apache 2.0 License'){
+    return '![License](https://img.shields.io/badge/Lincense-Apache_2.0-blue.svg';
+  } else if (license === 'GNU GPL v2'){
+    return '![License](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+  } else if (license === 'AGPL v3')
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,6 +19,45 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+## Description
+
+${data.description}
+
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Credits
+
+Github Username: ${data.github}
+
+Github Link: ${data.githubLink}
+
+## License
+
+${data.license}
+
+## How to Contribute
+
+${data.contributions}
+
+## Tests
+
+${data.test}
 
 `;
 }
